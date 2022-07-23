a7b473648311da07ade73ae88eda602e52e06ed7
Rebecca <njokinyagah@gmail.com>
2022-07-23 14:55:00 +0300

    updated configfile

--- _config.js
+++ _config.js
@@ -3,8 +3,8 @@ var config = {}
 // Update to have your correct username and password
 config.mongoURI = {
     production: 'mongodb+srv://Becca22:<Ambalicious40405!>@gallery-cluster.4qjvt.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://Becca22:<password>@gallery-cluster.4qjvt.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://Becca22:<password>@gallery-cluster.4qjvt.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://Becca22:<Ambalicious40405!>@gallery-cluster.4qjvt.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://Becca22:<Ambalicious40405!>@gallery-cluster.4qjvt.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
 
