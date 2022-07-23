2dac7b3a3ead036a0bd9feffbfa2fa3f27242474
Rebecca <njokinyagah@gmail.com>
2022-07-23 12:41:35 +0300

    updated configfile

--- _config.js
+++ _config.js
@@ -2,8 +2,9 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://Becca22:<Ambalicious40405!>@gallery-cluster.4qjvt.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://Becca22:<password>@gallery-cluster.4qjvt.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://Becca22:<password>@gallery-cluster.4qjvt.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
+
